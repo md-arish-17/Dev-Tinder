c5466b2dd92cbee24d82fddfe0a1887ae760e67d
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-02 00:23:16 +0530

    exploring get,post and delete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const app = express()
 
+app.get("/user",(req,res)=>{
+    res.send({name:"Arish",age:21})
+})
+
+app.post("/user",(req,res)=>{
+    res.send("Data Submitted")
+    console.log("Data Submitted Successfully in the Database")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data Deleted")
+    console.log("Data Deleted Successfully")
+})
+
+app.use("/hello/2",(req,res)=>{
+    res.send("usman hello bol rhe hai")
+})
+
 app.use("/hello",(req,res)=>{
     res.send("hello hello hello")
 })
