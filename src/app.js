5d49819dcc647f151cfb67438cf8b96330461331
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-02 02:52:08 +0530

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 const express = require("express")
 const app = express()
 
-app.get("/user/:userId/:userName/:password",(req,res)=>{
-    console.log(req.params)
-    res.send({firstName:"Arish",age:21})
-})
+app.use("/user",
+    (req,res,next)=>{
+        console.log("Route 1 Called")
+        next()
+        // res.send("Response 1")
+},
+    (req,res,next)=>{
+        console.log("Response 2 called")
+        // res.send("Response 2")
+        next()
+    },
+    (req,res,next)=>{
+        console.log("Response 3 Called")
+        next()
+    },
+    (req,res)=>{
+        console.log("Response 4 called")
+        res.send("Response 4")
+    }
+)
 
 app.listen(8081,()=>{
     console.log("Port running at 8081") 
