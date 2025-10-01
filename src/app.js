d6ad89db43b6d664b9dea36861494cf32731ceaa
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-10-02 03:17:16 +0530

    Middleware

--- src/app.js
+++ src/app.js
@@ -1,26 +1,18 @@
 const express = require("express")
 const app = express()
 
-app.use("/user",
-    (req,res,next)=>{
-        console.log("Route 1 Called")
-        next()
-        // res.send("Response 1")
-},
-    (req,res,next)=>{
-        console.log("Response 2 called")
-        // res.send("Response 2")
-        next()
-    },
-    (req,res,next)=>{
-        console.log("Response 3 Called")
-        next()
-    },
-    (req,res)=>{
-        console.log("Response 4 called")
-        res.send("Response 4")
-    }
-)
+app.use("/",(req,res,next)=>{
+    console.log("Response 1 is here")
+    next()
+})
+
+app.use("/user",(req,res,next)=>{
+    console.log("Response 2")
+    next()
+},(req,res,next)=>{
+    console.log("Response 3")
+    res.send("Response 3")
+})
 
 app.listen(8081,()=>{
     console.log("Port running at 8081") 
